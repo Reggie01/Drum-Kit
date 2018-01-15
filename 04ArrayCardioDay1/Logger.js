cab3105be13f33b22bd0680297c5c914c24c1cf9
Reggie S <reggie@bgcreatives.com>
2018-01-14 23:32:11 -0500

    fixed sort functions, removed console.log statements

---
+++ 04ArrayCardioDay1/Logger.js
@@ -0,0 +1,27 @@
+﻿let DEBUGGER = true;
+let LOGGER = {
+  createHeader : function( header ) {
+      return `|${header}|\n`;
+  },
+  createHeading : function( heading ) {
+    let border = "+----------------------------------+---------+------------------------+----------------+\n";
+    let pad      = "                                                                                                                            ";
+    let borderLength = border.length;
+    let borderPadding = function( msg ) {
+             // leave room for beginning and ending border "|"
+             // and minus "\n" nextline string 
+             // |blue      |
+             return ( msg + pad ).slice( 0, borderLength - 3 );       
+    }
+    let paddedHeader = borderPadding( heading );
+    
+    if( DEBUGGER ) {
+        console.log( "%cPadded Header : " + paddedHeader, 'background: #222; color: #bada55' );
+        console.log( this );
+    } 
+    return border + this.createHeader( paddedHeader ) + border;
+  },
+  setDebugger : function( bool ) {
+      DEBUGGER = bool;
+  }
+};
