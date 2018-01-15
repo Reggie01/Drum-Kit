3eca8581e1d66e16a6d28d5cbe25ec03f9e98c5b
Reggie S <reggie@bgcreatives.com>
2018-01-15 13:41:22 -0500

    updated readme, started flex panel solution
    spa.model = ( function() {
      // ------------------------ Begin Module Scope Variables --------------
      var
        configMap = {
          settable_map : { color_name: true },
          color_name    : "blue"
        },
        stateMap = {
          $container : null
        },
        jqueryMap = {},
    
        setJqueryMap, configModule, initModule;
      //---------------- END MODULE SCOPE VARIABLES ---------------
      //---------------- BEGIN UTILITY METHODS --------------------
      // Returns copy of stored anchor map; minimizes overhead
        example: getTrimmedString
      //---------------- END UTILITY METHODS ----------------------
      //---------------- BEGIN DOM METHODS ------------------------
    
      // Begin DOM method /setJqueryMap/
        setJqueryMap = function() {
          var $container = stateMap.$container;
          jqueryMap = {
            $container : $container
          };
        };
      // End DOM method /setJqueryMap/
      //----------------- End DOM METHODS --------------------------
      //--------------- BEGIN EVENT HANDLERS -----------------------
      // example: onClickButton = . . .
      //--------------- END EVENT HANDLERS -------------------------
      //------------------ BEGIN PUBLIC METHODS --------------------
      // Begin public method /configModule/
      // Purpose      : Adjust configuration of allowed keys
      // Arguments  : A map of settable keys and values
      //    * color_name - color to use
      // Settings      :
      //    * configMap.settable_map declares allowed keys
      // Returns      : true
      // Throws       : none
      //
      configModule = function ( input_map ) {
        spa.butil.setConfigMap( {
          input_map      : input_map,
          settable_map :  configMap.settable_map,
          config_map    : configMap
        });
        return true;

---
+++ Logger.js
@@ -0,0 +1,36 @@
+﻿
+let LOGGER = ( function( options ) {
+  let DEBUGGER = true;
+  let options = {
+      
+  }
+  let createHeader = function( header ) {
+      return `|${header}|\n`;
+  };
+  let createHeading = function( heading ) {
+    let border = "+----------------------------------+---------+------------------------+----------------+\n";
+    let pad      = "                                                                                                                            ";
+    let borderLength = border.length;
+    let borderPadding = function( msg ) {
+             // leave room for beginning and ending border "|"
+             // and minus "\n" nextline string 
+             // |blue      |
+             return ( msg + pad ).slice( 0, borderLength - 3 );       
+    }
+    let paddedHeader = borderPadding( heading );
+    
+    if( DEBUGGER ) {
+        console.log( "%cPadded Header : " + paddedHeader, 'background: #222; color: #bada55' );
+        console.log( this );
+    } 
+    return border + this.createHeader( paddedHeader ) + border;
+  };
+  let setDebugger = function( bool ) {
+      this.options.DEBUGGER = bool;
+  }
+  return {
+      createHeading : createHeading,
+      setDegger : setDebugger
+  }
+  }
+})( options );
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# Drum-Kit" 
+#Javascript30 Solution Set
+
+I am solving the coding challenges from Wes Bos's [Javascript30](https://javascript30.com/). If you are interested in solving easy - medium difficulty dom interface and javascript problems this is the course for you.
